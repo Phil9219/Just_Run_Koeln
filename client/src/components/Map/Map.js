075da44041555de46ecab6d113273d8f7e429f91
Phil9219 <philipp.fell@yahoo.de>
2020-12-02 23:32:59 +0100

    Add panTo function for location on map

--- client/src/components/Map/Map.js
+++ client/src/components/Map/Map.js
@@ -1,4 +1,5 @@
 import React from "react";
+
 import {
   GoogleMap,
   useLoadScript,
@@ -7,6 +8,17 @@ import {
 } from "@react-google-maps/api";
 
 import MapStyle from "./MapStyle";
+import usePlacesAutocomplete, {
+  getGeocode,
+  getLatLng,
+} from "use-places-autocomplete";
+import {
+  Combobox,
+  ComboboxInput,
+  ComboboxPopover,
+  ComboboxList,
+  ComboboxOption,
+} from "@reach/combobox";
 import { formatRelative } from "date-fns";
 
 const libraries = ["places"];
@@ -49,11 +61,18 @@ export default function App() {
     mapRef.current = map;
   }, []);
 
+  const panTo = React.useCallback(({ lat, lng }) => {
+    mapRef.current.panTo({ lat, lng });
+    mapRef.current.setZoom(14);
+  }, []);
+
   if (loadError) return "Error loading maps";
   if (!isLoaded) return "Loading Maps";
 
   return (
     <div>
+      <Search panTo={panTo} />
+
       <GoogleMap
         mapContainerStyle={mapContainerStyle}
         zoom={8}
@@ -87,5 +106,53 @@ export default function App() {
         ) : null}
       </GoogleMap>
     </div>
+  );
+}
+
+function Search(panTo) {
+  const {
+    ready,
+    value,
+    suggestions: { status, data },
+    setValue,
+    clearSuggestions,
+  } = usePlacesAutocomplete({
+    requestOptions: {
+      location: { lat: () => 50.937531, lng: () => 6.960279 },
+      radius: 200 * 1000,
+    },
+  });
+
+  return (
+    <Combobox
+      onSelect={async (address) => {
+        setValue(address, false);
+        clearSuggestions();
+        try {
+          const results = await getGeocode({ address });
+          const { lat, lng } = await getLatLng(results[0]);
+          panTo({ lat, lng });
+        } catch (error) {
+          console.log("Error: ", error);
+        }
+      }}
+    >
+      <ComboboxInput
+        value={value}
+        onChange={(e) => {
+          setValue(e.target.value);
+        }}
+        disabled={!ready}
+        placeholder="Choose StartingPoint"
+      />
+      <ComboboxPopover>
+        <ComboboxList>
+          {status === "OK" &&
+            data.map(({ id, description }) => (
+              <ComboboxOption key={id} value={description} />
+            ))}
+        </ComboboxList>
+      </ComboboxPopover>
+    </Combobox>
   );
 }

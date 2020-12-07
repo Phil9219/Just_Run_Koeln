fe0912da38566b494bf2c553c3886bf68ba47137
Phil9219 <philipp.fell@yahoo.de>
2020-12-07 15:04:42 +0100

    Add routing to App.js

--- client/src/App.js
+++ client/src/App.js
@@ -1,19 +1,40 @@
 import React from "react";
-// import LoginPage from "../src/components/Page/LoginPage";
 import GlobalStyle from "./GlobalStyle";
-// import Datepicker from "../src/components/Calendar/Calendar";
-// import ChooseYourRunPage from "../src/components/Page/ChooseYourRunPage";
-// import CreatedRunPage from "./components/Page/CreatedRunPage";
-// import RunsDonePage from "./components/Page/RunsDonePage";
+import SplashPage from "../src/components/Page/SplashPage";
+import LoginPage from "../src/components/Page/LoginPage";
+import ChooseYourRunPage from "../src/components/Page/ChooseYourRunPage";
+import CreatedRunPage from "./components/Page/CreatedRunPage";
+import RunsDonePage from "./components/Page/RunsDonePage";
 import RunSetupPage from "./components/Page/RunSetupPage";
+import { BrowserRouter as Router, Switch, Route } from "react-router-dom";
 
 function App() {
   return (
     <>
-      <GlobalStyle />
-      <div>
-        <RunSetupPage />
-      </div>
+      <Router>
+        <GlobalStyle />
+
+        <Switch>
+          <Route exact path="/splash">
+            <SplashPage />
+          </Route>
+          <Route path="/login">
+            <LoginPage />
+          </Route>
+          <Route path="/choose your run">
+            <ChooseYourRunPage />
+          </Route>
+          <Route path="/setup run">
+            <RunSetupPage />
+          </Route>
+          <Route path="/created run">
+            <CreatedRunPage />
+          </Route>
+          <Route path="/runs done">
+            <RunsDonePage />
+          </Route>
+        </Switch>
+      </Router>
     </>
   );
 }
